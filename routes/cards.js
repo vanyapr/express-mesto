8f12a03d82a1db1062c0e5991e418ab58c965ef8
Ivan Proskuryakov <snowflax@ya.ru>
2020-10-11 02:56:51 +0300

    refactor: Внести правки в соответствии с замечаниями код-ревью

--- routes/cards.js
+++ routes/cards.js
@@ -1,22 +1,28 @@
 const fs = require('fs'); // Подключили файловую систему
 const path = require('path'); // Подключили модуль path
 const jsonUrl = path.join(__dirname, '../data/cards.json'); // Вычисляем урл адрес файла
-
-let cardsJSON = {};
-
-// Читаем файл по урл адресу
-fs.readFile(jsonUrl, { encoding: 'utf-8' }, (error, data) => {
-  if (error) {
-    console.log(error);
-  }
-
-  // Полученный поток преобразовали в данные JSON
-  cardsJSON = JSON.parse(data);
-});
+let cardsJSON = {}; // Назначили переменную для записи данных ответа
+let status = 200; // Назначили статус ответа по умолчанию
 
 // Вернули список карточек
 const getCards = (req, res) => {
-  res.send(cardsJSON);
+  // Читаем файл
+  fs.readFile(jsonUrl, { encoding: 'utf-8' }, (error, data) => {
+    // Отлавливаем возможные ошибки чтения
+    try {
+      if (error) {
+        throw error;
+      }
+
+      // Полученный поток преобразовали в данные JSON
+      cardsJSON = JSON.parse(data);
+    } catch (err) {
+      cardsJSON = { message: 'Ошибка на сервере' };
+      status = 500;
+    } finally {
+      res.status(status).send(cardsJSON);
+    }
+  });
 };
 
 module.exports = getCards;
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,54 @@
 // const users = require('../data/users.json'); // Подключили JSON файл с пользователями
-const fs = require('fs');
-const path = require('path');
-const jsonPath = path.join(__dirname, '../data/users.json');
-
+const fs = require('fs'); // Файловая система
+const path = require('path'); // Метод path
+const jsonPath = path.join(__dirname, '../data/users.json'); // Подключили данные
 let users = {}; // Создали пустой объект с пользователями
-
-fs.readFile(jsonPath, { encoding: 'utf-8' }, (error, data) => {
-  if (error) {
-    console.log(error);
-  }
-
-  users = JSON.parse(data);
-});
+let status = 200; // Задали статус ответа по умолчанию
+let user = '';
 
 // Отдаём список пользователей
 const getUsersList = (req, res) => {
-  res.send(users);
+  fs.readFile(jsonPath, { encoding: 'utf-8' }, (error, data) => {
+    try {
+      if (error) {
+        throw error;
+      }
+
+      users = JSON.parse(data);
+    } catch (err) {
+      users = { message: 'Ошибка на сервере' };
+      status = 500;
+    } finally {
+      res.status(status).send(users);
+    }
+  });
 };
 
 // Проверяем, существует ли юзер с таким айди?
 const getUser = (req, res, next) => {
-  // Ищем юзера в базе данных
-  const user = users.find((item) => item._id === req.params.id);
+  fs.readFile(jsonPath, { encoding: 'utf-8' }, (error, data) => {
+    try {
+      if (error) {
+        throw error;
+      }
 
+      users = JSON.parse(data);
+
+      if (status === 200) {
+        // Ищем юзера в базе данных
+        user = users.find((item) => item._id === req.params.id);
+      }
+    } catch (err) {
+      /* Поскольку мы не проходили блоки try...catch, попробую наколхозить */
+      user = { message: 'Ошибка на сервере' };
+      // throw error выбросит ошибку в ноду, поэтому его нельзя использовать
+      status = 500;
+    }
+  });
+
+  // Если юзер есть, возвращаем его
   if (user) {
-    res.send(user);
+    res.status(status).send(user);
     return; // Не забывать выходить из функции, если достигнуто нужное условие
   }
 
