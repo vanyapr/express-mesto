70ade73aeb327e50b1c885a28d6b2f792e2ca17d
Ivan Proskuriakov <snowflax@ya.ru>
2020-10-10 18:04:40 +0300

    refactor: Отключить в ESLINT проверку приватных полей класса

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ function foo(c) {
 }
 
 foo(varName);
+
+class Testing {
+  constructor(prop) {
+    this._test = prop.test;
+  }
+
+  doStuff() {
+    console.log(this._test);
+  }
+}
+
+const test = new Testing('Vasiliy');
+
+test.doStuff();
