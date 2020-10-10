e3433c6b921de9cec494ae3b038a853252d2ad55
Ivan Proskuriakov <snowflax@ya.ru>
2020-10-10 20:25:57 +0300

    refactor: Реализовать чтение файла с данными из файловой системы в соотвествтии с чеклистом

--- router.js
+++ router.js
@@ -3,7 +3,6 @@ const getCards = require('./routes/cards'); // Подключили мидллв
 const { getUsersList, getUser, userNotFound } = require('./routes/users'); // Роут пользоватетей
 const notFound = require('./routes/notFound');
 
-
 // Если урл адрес существует, вернуть юзера или список
 router.get('/cards', getCards);
 router.get('/users/', getUsersList);
--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,20 @@
-const cardsJSON = require('../data/cards.json');
+const fs = require('fs'); // Подключили файловую систему
+const path = require('path'); // Подключили модуль path
+const jsonUrl = path.join(__dirname, '../data/cards.json'); // Вычисляем урл адрес файла
 
+let cardsJSON = {};
+
+// Читаем файл по урл адресу
+fs.readFile(jsonUrl, { encoding: 'utf-8' }, (error, data) => {
+  if (error) {
+    console.log(error);
+  }
+
+  // Полученный поток преобразовали в данные JSON
+  cardsJSON = JSON.parse(data);
+});
+
+// Вернули список карточек
 const getCards = (req, res) => {
   res.send(cardsJSON);
 };
